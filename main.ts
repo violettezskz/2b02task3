f2dc5b997695b135392860111a2390b362f3d7e6
violettezskz <115679845+violettezskz@users.noreply.github.com>
2022-10-13 11:04:57 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+for (let index = 0; index <= 3; index++) {
+    basic.showNumber(3 - index)
+}
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # . # .
+    . . # . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . # . # .
+    # . . . #
+    . . . . .
+    # . . . #
+    . # . # .
+    `)
+basic.showLeds(`
+    # . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . #
+    `)
