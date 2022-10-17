abdb236b26dd0a1bbd9165d9ce629e308cf405e0
violettezskz <115679845+violettezskz@users.noreply.github.com>
2022-10-17 23:01:22 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,52 @@
-for (let index = 0; index <= 3; index++) {
-    basic.showNumber(3 - index)
-}
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . # . # .
-    . . # . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . # . .
-    . # . # .
-    # . . . #
-    . # . # .
-    . . # . .
-    `)
-basic.showLeds(`
-    . # . # .
-    # . . . #
-    . . . . .
-    # . . . #
-    . # . # .
-    `)
-basic.showLeds(`
-    # . . . #
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . #
-    `)
+input.onButtonPressed(Button.A, function () {
+    if (Math.randomBoolean()) {
+        if (true) {
+            for (let index = 0; index <= 3; index++) {
+                basic.showNumber(3 - index)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . # . .
+                    . # . # .
+                    . . # . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . # . .
+                    . # . # .
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    `)
+                basic.showLeds(`
+                    . # . # .
+                    # . . . #
+                    . . . . .
+                    # . . . #
+                    . # . # .
+                    `)
+                basic.showLeds(`
+                    # . . . #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # . . . #
+                    `)
+            }
+        }
+        if (false) {
+            led.stopAnimation()
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (true) {
+        basic.showNumber(0)
+    }
+})
